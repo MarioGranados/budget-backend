278c7b9db8715597f83437720623af7bd1248e9a
Mario Granados <mariogranados@Marios-MBP.attlocal.net>
2025-02-03 00:31:20 -0600

    verficiation stuff

--- services/mailer.js
+++ services/mailer.js
@@ -8,7 +8,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendVerificationEmail = (toEmail, verificationCode) => {
   const msg = {
     to: toEmail, // Recipient email address
-    from: 'thecloudydeveloper@gmail.com', // Verified sender email (could be any custom domain you verified in SendGrid)
+    from: process.env.SENDGRID_EMAIL, // Verified sender email (could be any custom domain you verified in SendGrid)
     subject: 'Email Verification',
     text: `Your verification code is: ${verificationCode}`,
   };
